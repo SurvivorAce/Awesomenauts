8b9ef67350cc35362c9d897cf168f68a6e7cf5e7
Anderson Nguyen <andersonnguyen126@gmail.com>
2015-02-05 23:06:03 -0800

    video 15. Colliding with Bases~

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -12,6 +12,8 @@ game.PlayerEntity = me.Entity.extend({
 		}]);
 		//Sets current position
 		this.body.setVelocity(5, 20);
+		//*Keeps track of which direction your character is going
+		this.facing = "right";
 		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
 
 		this.renderable.addAnimation("idle", [78]);
@@ -28,9 +30,11 @@ game.PlayerEntity = me.Entity.extend({
 			//setVelocity() and multiplying it by me.timer.tick 
 			//me.timer.tick makes movement look smooth*/
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
+			this.facing = "right";
 			this.flipX(false);
 		}
 		else if(me.input.isKeyPressed("left")){
+			this.facing = "left";
 			this.body.vel.x -= this.body.accel.x * me.timer.tick;
 			this.flipX(true);
 		}
@@ -38,8 +42,8 @@ game.PlayerEntity = me.Entity.extend({
 			this.body.vel.x = 0;
 		}		
 
-		if(me.input.isKeyPressed("jump") && !this.jumping && !this.falling){
-			this.jumping = true;
+		if(me.input.isKeyPressed("jump") && !this.body.jumping && !this.body.falling){
+			this.body.jumping = true;
 			this.body.vel.y -= this.body.accel.y * me.timer.tick;
 		}
 
@@ -76,11 +80,31 @@ game.PlayerEntity = me.Entity.extend({
 			}
 		}		
 
+		me.collision.check(this, true, this.collideHandler.bind(this), true);
 		this.body.update(delta);
 
 		this._super(me.Entity, "update", [delta]);
 		return true;
+	},
+
+	collideHandler: function(response){
+		if (response.b.type==='EnemyBaseEntity') {
+			var ydif = this.pos.y - response.b.pos.y;
+			var xdif = this.pos.x - response.b.pos.x;
+
+			if(xdif>-35 && this.facing==='right' && (xdif<0)){
+				this.body.vel.x = 0;
+				this.pos.x = this.pos.x -1;
+			}
+			else if(xdif<70 && this.facing==='left' && (xdif>0)){
+				this.body.vel.x = 0;
+				this.pos.x = this.pos.x +1;
+			}
+
+		}
 	}
+
+
 });
 
 game.PlayerBaseEntity = me.Entity.extend ({
