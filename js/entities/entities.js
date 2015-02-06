e8adb721e20a1fe14a26d3ad9a1641f5a12b9ea1
Anderson Nguyen <andersonnguyen126@gmail.com>
2015-02-05 23:04:55 -0800

    video 14. Moving left and right~

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -28,9 +28,19 @@ game.PlayerEntity = me.Entity.extend({
 			//setVelocity() and multiplying it by me.timer.tick 
 			//me.timer.tick makes movement look smooth*/
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
+			this.flipX(false);
+		}
+		else if(me.input.isKeyPressed("left")){
+			this.body.vel.x -= this.body.accel.x * me.timer.tick;
+			this.flipX(true);
 		}
 		else{
 			this.body.vel.x = 0;
+		}		
+
+		if(me.input.isKeyPressed("jump") && !this.jumping && !this.falling){
+			this.jumping = true;
+			this.body.vel.y -= this.body.accel.y * me.timer.tick;
 		}
 
 		if(me.input.isKeyPressed("attack")){
@@ -82,7 +92,7 @@ game.PlayerBaseEntity = me.Entity.extend ({
 			spritewidth: "100",
 			spriteheight: "100",
 			getShape: function() {
-				return (new me.Rect(0, 0, 100, 100)).toPolygon();
+				return (new me.Rect(0, 0, 100, 80)).toPolygon();
 			}
 		}]);
 
@@ -127,7 +137,7 @@ game.EnemyBaseEntity = me.Entity.extend ({
 			spritewidth: "100",
 			spriteheight: "100",
 			getShape: function() {
-				return (new me.Rect(0, 0, 100, 100)).toPolygon();
+				return (new me.Rect(0, 0, 100, 80)).toPolygon();
 			}
 		}]);
 
