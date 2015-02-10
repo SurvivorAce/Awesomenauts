6cd5ae47929e009514bbdb88d2ee8611f3ba4640
Anderson Nguyen <andersonnguyen126@gmail.com>
2015-02-10 12:21:17 -0800

    video 19. ADDED CREEPE(CREEPS!) ON A TIMER~

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -220,7 +220,7 @@ game.EnemyCreep = me.Entity.extend({
 			this.health = 10;
 			this.alwaysUpdate = true;
 
-			this.setVeloctiy(3, 20);
+			this.body.setVelocity(3, 20);
 
 			this.type = "EnemyCreep";
 
@@ -231,4 +231,25 @@ game.EnemyCreep = me.Entity.extend({
 
 			}
 			
+});
+
+game.GameManager = Object.extend({
+	init: function(x, y, settings){
+		this.now = new Date() .getTime();
+		this.lastCreep = new Date() .getTime();
+
+		this.alwaysUpdate = true;
+	},
+
+	update: function(){
+		this.now = new Date() .getTime();
+
+		if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
+			this.lastCreep = this.now;
+			var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
+			me.game.world.addChild(creepe, 5);
+		}
+
+		return true;
+	}	
 });
