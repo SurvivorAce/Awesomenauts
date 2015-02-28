c78c6502c93251aacea0288028629f3dc341e97a
Anderson Nguyen <andersonnguyen126@gmail.com>
2015-02-27 19:12:26 -0800

    video 33. REFACTORING COLLISION!~

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -138,8 +138,16 @@ game.PlayerEntity = me.Entity.extend({
 		this.health = this.health - damage;
 	},
 
-	collideHandler: function(response){
+	collideHandler: function(response) {
 		if (response.b.type==='EnemyBaseEntity') {
+			this.collideWithEnemyBase(response);
+		}
+		else if(response.b.type==='EnemyCreep') {
+			this.collideWithEnemyCreep(response);
+		}
+	},
+
+	collideWithEnemyBase: function(response) {
 			var ydif = this.pos.y - response.b.pos.y;
 			var xdif = this.pos.x - response.b.pos.x;
 
@@ -149,52 +157,60 @@ game.PlayerEntity = me.Entity.extend({
 			}
 			else if(xdif>-35 && this.facing==='right' && (xdif<0)){
 				this.body.vel.x = 0;
-				//this.pos.x = this.pos.x -1;
 			}
 			else if(xdif<70 && this.facing==='left' && (xdif>0)){
 				this.body.vel.x = 0;
-				//this.pos.x = this.pos.x +1;
 			}
-
-
 			if(this.renderable.isCurrentAnimation("attack") && this.now - this.lastHit >= game.data.playerAttackTimer) {
 				this.lastHit = this.now;
 				response.b.loseHealth(game.data.playerAttack);
 			}
-		}
-		else if(response.b.type==='EnemyCreep') {
-			var xdif = this.pos.x - response.b.pos.x;
-			var ydif = this.pos.y - response.b.pos.y;
+	},
 
-			if(xdif>0) {
-				//this.pos.x = this.pos.x + 1;
-				if(this.facing==="right"){
-					this.body.vel.x = 0;
-				}
-			}
-			else {
-				//this.pos.x = this.pos.x - 1;
-				if(this.facing==="left"){
-					this.body.vel.x = 0;
-				}
-			}
+	collideWithEnemyCreep: function(response) {
+		var xdif = this.pos.x - response.b.pos.x;
+		var ydif = this.pos.y - response.b.pos.y;
 
-			if (this.renderable.isCurrentAnimation('attack') && this.now-this.lastHit >= game.data.playerAttackTimer && (Math.abs(ydif<=40) && 
-				((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing==="right")
-				)){
-				this.lastHit = this.now;
-				//if the creeps health is less than our attack, execute code in if statement
-				if(response.b.health <= game.data.playerAttack) {
-					//adds one gold for a creep kill
-					game.data.gold += 1;
-					console.log("Current gold: " + game.data.gold);
-				}
+		this.stopMovement(xdif);
 
-				response.b.loseHealth(game.data.playerAttack1);
+		if (this.checkAttack(xdif, ydif)) {
+			this.hitCreep(response);	
+		};
+	},
+
+	stopMovement: function(xdif) {
+		if(xdif>0) {
+			if(this.facing==="right"){
+				this.body.vel.x = 0;
 			}
 		}
+		else {
+			if(this.facing==="left"){
+				this.body.vel.x = 0;
+			}
+		}		
+	},
+
+	checkAttack: function(xdif, ydif) {
+		if (this.renderable.isCurrentAnimation('attack') && this.now-this.lastHit >= game.data.playerAttackTimer && (Math.abs(ydif<=40) && 
+			((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing==="right")
+			)){
+			this.lastHit = this.now;
+			//if the creeps health is less than our attack, execute code in if statement
+			return true;
+		}		
+		return false;
+	},
+
+	hitCreep: function(response) {
+		if(response.b.health <= game.data.playerAttack) {
+			//adds one gold for a creep kill
+			game.data.gold += 1;
+			console.log("Current gold: " + game.data.gold);
+		}
+
+		response.b.loseHealth(game.data.playerAttack1);				
 	}
-
 
 });
 
