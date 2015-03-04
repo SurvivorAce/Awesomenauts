6308a20eeaddfc4f0e009df35e65dce811d33113
Anderson Nguyen <andersonnguyen126@gmail.com>
2015-03-04 11:50:32 -0800

    video 37. CHECKING AND FIXING EXPERIENCE MANAGER!~

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -49,15 +49,19 @@ game.HeroDeathManager = Object.extend({
 game.ExperienceManager = Object.extend({
 	init: function(x, y, settings) {
 		this.alwaysUpdate = true;
+		this.gameOver = false;
 	},
 
 	update: function() {
 		if(game.data.win === true) {
 			game.data.exp += 10;
+			this.gameOver = true;
 		}
 		else if(game.data.win === false) {
 			game.data.exp += 1;
+			this.gameOver = true;
 		}
+		console.log(game.data.exp);
 		
 		return true;
 	}
